df34198b0e8cba5a9794535eac46c17f697d8cfc
Sachin-nsk <sachinkumarnsk20090@gmail.com>
2024-09-01 19:45:39 +0530

    Do the navigation bar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css'
+import logo from  '../../assets/logo.png';
+import signUpIcon from '../../assets/sign_up icon.png';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <i class="fa-brands fa-bitcoin fa-2xl"></i>
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>BLog</li>
+      </ul>
+      <div className="nav-right">
+        <select>
+            <option value="inr">INR</option>
+            <option value="usd">USD</option>
+            <option value="euro">EURO</option>
+            
+        </select>
+        <button>Sign up <img src={signUpIcon} alt="" /></button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
