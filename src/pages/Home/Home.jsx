a62c60af09ab8be4fc9795fbece70a6f8cba6c2d
Sachin-nsk <sachinkumarnsk20090@gmail.com>
2024-09-04 09:51:48 +0530

    Do the Home page - layout with css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,24 @@ import "./Home.css"
 
 const home = () => {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className='home'>
+      <div className='hero'>
+        <h1>Crypto Market</h1>
+        <p>Welcome to the cyptocurrency market. Sign up to explore more.</p>
+        <form>
+          <input type="text"  placeholder='Search Crypto'/>
+          <button type='submit'>Search</button>
+        </form>
+      </div>
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{textAlign:"center"}}>24Hr Change</p>
+          <p className='market-cap'>Market Cap</p>
+        </div>
+      </div>
     </div>
   )
 }
