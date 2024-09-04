94e363c1ad668b47937d11b15114600a78c38a3f
Sachin-nsk <sachinkumarnsk20090@gmail.com>
2024-09-04 10:37:38 +0530

    Implement currency fetch from api

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 import './Navbar.css'
 import signUpIcon from '../../assets/sign_up icon.png';
+import { CoinContext } from '../../context/CoinContext';
 
 const Navbar = () => {
+
+  const {setCurrency} = useContext(CoinContext)
+
+  const currencyHandler = (event)=>{
+    switch(event.target.value){
+      case "usd": {
+        setCurrency({name:"usd",symbol:"$"});
+        break;
+      }
+      case "eur": {
+        setCurrency({name:"eur",symbol:"eu"});
+        break;
+      }
+      case "inr": {
+        setCurrency({name:"inr",symbol:"ru"});
+        break;
+      }
+      default: {
+        setCurrency({name:"usd",symbol:"$"});
+        break;
+      }
+    }
+  }
+
   return (
     <div className='navbar'>
       <i class="fa-brands fa-bitcoin fa-2xl" className='logo'></i>
@@ -13,7 +38,7 @@ const Navbar = () => {
         <li>BLog</li>
       </ul>
       <div className="nav-right">
-        <select>
+        <select onChange={currencyHandler}>
             <option value="inr">INR</option>
             <option value="usd">USD</option>
             <option value="euro">EURO</option>
