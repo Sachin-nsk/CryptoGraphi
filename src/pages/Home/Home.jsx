0b90d0f9c3b38a2fef856368112fe3893fd4f421
Sachin-nsk <sachinkumarnsk20090@gmail.com>
2024-09-09 11:14:13 +0530

    Fix display slice error

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -48,7 +48,7 @@ const home = () => {
           <p className='market-cap'>Market Cap</p>
         </div>
         {
-          displayCoin.slice(0,10).map((item,index)=>(
+          Array.isArray(displayCoin) && displayCoin.slice(0, 10).map((item, index) =>(
             <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
               <p>{item.market_cap_rank}</p>
               <div>
